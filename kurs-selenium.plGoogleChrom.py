589943fa513dc9464aeceab65111a4b96f21d4fb
Paweł Zborowski <P.Zborowski@vector.net>
2020-12-11 18:05:38 +0100

    Test wyszukiwania pobytu dla czterech osób dorosłych

---
+++ kurs-selenium.plGoogleChrom.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.implicitly_wait(10)
+driver.maximize_window()
+driver.get("http://www.kurs-selenium.pl/demo/")
+driver.find_element_by_xpath("//span[text()='Search by Hotel or City Name']").click()
+driver.find_element_by_xpath("//div[@id='select2-drop']//input").send_keys('Dubai')
+driver.find_element_by_xpath("//span[text()='Dubai']").click()
+driver.find_element_by_name("checkin").send_keys("12/09/2020")
+driver.find_element_by_name("checkout").send_keys("13/09/2020")
+driver.find_element_by_id("travellersInput").click()
+driver.find_element_by_id("adultInput").clear()
+driver.find_element_by_id("adultInput").send_keys("4")
+driver.find_element_by_xpath("//button[text()=' Search']").click()
+driver.close()
